424dc5ed365fd4af7aa949def668300ce41f2b96
Chandu291206 <ommichanduchethan2006@gmail.com>
2025-10-05 23:32:47 +0530

    last

--- server/index.js
+++ server/index.js
@@ -10,7 +10,8 @@ const mongoURI = 'mongodb+srv://vedasree:Vedha%4018052007@cluster0.6sdzthg.mongo
 mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
-  
+
+
 
 app.listen(3001, () => {
   console.log('Server is running on http://localhost:3001');
